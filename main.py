d81ea1c98e664430f25f237ea874f8ec25478453
Zixer3 <90540211+Zixer3@users.noreply.github.com>
2022-05-08 13:17:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,108 @@
+
+import pygame as pg
+import pymunk.pygame_util
+
+pymunk.pygame_util.positive_y_is_up = False
+
+
+clock = pg.time.Clock()
+
+body = pymunk.Body()
+ball_mass, ball_radius = 1, 10
+class Game:
+    pg.init()
+    def __init__(self):
+        self.name_game = pg.image.load('Interface_pictures/name.png')
+        self.name_game_pos = (0, 0)
+        self.start_button = pg.image.load('Interface_pictures/start.png')
+        self.start_button_pos = (0, 100)
+
+        self.WIDTH = 1280
+        self.HEIGHT = 720
+        self.FPS = 100
+        self.surface = pg.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.draw_options = pymunk.pygame_util.DrawOptions(self.surface)
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 8000
+
+        self.body = pymunk.Body()
+        self.ball_mass = 1
+        self.ball_radius = 10
+        self.active_window = 'menu'
+
+    def create_platform(self, space):
+        segment_shape_1 = pymunk.Segment(space.static_body, (2, self.HEIGHT), (self.WIDTH, self.HEIGHT), 26)
+        space.add(segment_shape_1)
+        segment_shape_1.elasticity = 0.0
+        segment_shape_1.friction = 1.0
+        segment_shape_2 = pymunk.Segment(space.static_body, (2, 2), (self.WIDTH, 2), 26)
+        space.add(segment_shape_2)
+        segment_shape_2.elasticity = 0.0
+        segment_shape_2.friction = 1.0
+        segment_shape_3 = pymunk.Segment(space.static_body, (self.WIDTH, 2), (self.WIDTH, self.HEIGHT), 26)
+        space.add(segment_shape_3)
+        segment_shape_3.elasticity = 0.0
+        segment_shape_3.friction = 1.0
+        segment_shape_4 = pymunk.Segment(space.static_body, (2, 2), (2, self.HEIGHT), 26)
+        space.add(segment_shape_4)
+        segment_shape_4.elasticity = 0.0
+        segment_shape_4.friction = 1.0
+
+    def create_ball(self, space, pos):
+        ball_moment = pymunk.moment_for_circle(ball_mass, 0, ball_radius)
+        ball_body = pymunk.Body(ball_mass, ball_moment)
+        ball_body.position = pos
+        ball_shape = pymunk.Circle(ball_body, ball_radius)
+        ball_shape.elasticity = 0.6
+        ball_shape.friction = 0.3
+        space.add(ball_body, ball_shape)
+        return ball_body
+
+    def try_to_click(self, pos):
+        if self.active_window == 'menu':
+            if 100 < pos[0] < 1280 and 0 < pos[1] < 720:
+                self.active_window = 'game'
+        if self.active_window == 'game':
+            self.create_ball(self.space, pos)
+
+    def menu_draw(self):
+        self.surface.fill((0, 0, 0))
+        self.surface.blit(self.name_game, self.name_game_pos)
+        self.surface.blit(self.start_button, self.start_button_pos)
+
+
+
+    def draw(self):
+        if self.active_window == 'game':
+            self.surface.fill((0, 0, 0))
+            self.create_platform(self.space)
+            for i in pg.event.get():
+                if i.type == pg.QUIT:
+                    exit()
+                if i.type == pg.MOUSEBUTTONDOWN:
+                    if i.button == 1:
+                        self.create_ball(self.space, i.pos)
+                        print(i.pos)
+
+            self.space.step(1 / self.FPS)
+            self.space.debug_draw(self.draw_options)
+        elif self.active_window == 'menu':
+            self.menu_draw()
+
+
+    def start(self):
+        self.draw()
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    return 0
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    self.try_to_click(event.pos)
+            self.draw()
+            pg.display.flip()
+
+
+game = Game()
+game.start()
+
